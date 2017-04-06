a8ca5353b09b58dba81c5f0c97b7a16b759e8b79
Juliane Seidl <seidl5@hm.edu>
2017-04-06 13:13:51 +0200

    Renderer render() Methode angefangen

--- src/main/java/edu/hm/renderer/Renderer.java
+++ src/main/java/edu/hm/renderer/Renderer.java
@@ -1,9 +1,47 @@
 package edu.hm.renderer;
 
+
+import java.lang.reflect.Field;
+
 public class Renderer {
     private Object object;
 
     public Renderer(Object object){
         this.object = object;
     }
+
+    public String render() throws IllegalAccessException {
+        // get the given objects class
+        Class objectClass = object.getClass();
+        // get the given objects class name
+        String output = "Instance of " + objectClass.getName() + ":\n";
+
+        // get all fields from the given objects
+        for(Field field : objectClass.getDeclaredFields()){
+            // including private fields
+            field.setAccessible(true);
+
+            // check if field has @RenderMe annotation
+            if(field.isAnnotationPresent(RenderMe.class)){
+                //todo test for with !!!!
+
+
+
+                // get field name, type and value according to instructions
+                output += field.getName() + "(Type " + field.getType().getCanonicalName() + "):" + field.get(objectClass) + "\n";
+
+
+            }
+
+
+
+
+        }
+
+
+
+        return "TO DO";
+    }
 }
+
+
